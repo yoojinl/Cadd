b2c6154c393830db26c8aadcc02900a3655bdc78
Rusty Robot <test0x0000@gmail.com>
2011-11-21 23:19:34 +0400

    small refactoring

--- cadd.py
+++ cadd.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import commands, sys, re, os, shutil
+import commands, sys, os, re, shutil
+from os import path
 from stat import *
 
 if len(sys.argv) < 3:
@@ -12,7 +13,7 @@ if len(sys.argv) < 3:
 file_name = sys.argv[1]
 dest_folder = sys.argv[2]
 
-if not os.path.isdir(dest_folder):
+if not path.isdir(dest_folder):
     print "Error:", dest_folder, "is not a directory."
     exit(1)
 
@@ -20,6 +21,7 @@ ldd_result = commands.getstatusoutput("ldd %s" % file_name)
 
 exit_code = ldd_result[0]
 if exit_code:
+    print "Command error: ldd %s" % file_name
     print "Error: exit code of ldd %s\n" % exit_code
     exit(1)
 
@@ -27,16 +29,15 @@ depends = re.findall('(/.*) ', ldd_result[1])
 
 print "Copy to", dest_folder
 for dep in depends:
-    shutil.copy(dep, os.path.join(dest_folder, os.path.split(dep)[1]))
+    shutil.copy(dep, path.join(dest_folder, path.basename(dep)))
     print "--", dep, '...'
 
 print "Copy", file_name
-dest_file_name = os.path.join(dest_folder, os.path.split(file_name)[1])
+dest_file_name = path.join(dest_folder, path.basename(file_name))
 shutil.copy(file_name, dest_file_name)
 
-
 script = "#!/bin/sh\n\
-env LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(dirname $(readlink -f $0)) $(dirname $(readlink -f $0))/%s $@\n" % os.path.split(file_name)[1]
+env LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(dirname $(readlink -f $0)) $(dirname $(readlink -f $0))/%s $@\n" % path.basename(file_name)
 
 script_name = dest_file_name + '.sh'
 f_script = open(script_name, 'w')
